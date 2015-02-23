f3ec87ddffb80d3f7b070f00d3b5ca45ee37519c
ststeiger <quandary82@hailmail.net>
2015-02-23 09:13:38 +0100

    References

--- SvgFloorPlanner/Properties/AssemblyInfo.cs
+++ SvgFloorPlanner/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern 
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyVersion("2015.2.20.1804")]
-[assembly: AssemblyFileVersion("2015.2.20.1804")]
+[assembly: AssemblyVersion("2015.2.23.0846")]
+[assembly: AssemblyFileVersion("2015.2.23.0846")]
